44564091d89f1302da8be3aa443cdec2be101a72
DevendraBhatta <devendrabhatta63@gmail.com>
2025-07-09 20:16:41 +0545

    commiting cool stuff in jetpack compose

--- ComposeApp/app/src/main/java/com/devdeveloper/composeapp/MainActivity.kt
+++ ComposeApp/app/src/main/java/com/devdeveloper/composeapp/MainActivity.kt
@@ -15,27 +15,39 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.devdeveloper.composeapp.ui.theme.MyTheme
+import com.devdeveloper.composeapp.ui.theme.lightGreen
+import com.devdeveloper.composeapp.ui.theme.skyBlue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MainScreen()
+            MyTheme {
+                MainScreen()
+            }
         }
     }
 }
@@ -43,22 +55,57 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(
 ) {
-    Surface(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        ProfileCard()
+    Scaffold(topBar = { AppBar() }) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            color = MaterialTheme.colorScheme.lightGreen
+        ) {
+            Column {
+
+                ProfileCard("Messi", true)
+                ProfileCard("Ronaldo", false)
+                ProfileCard("Neymar Junior", true)
+            }
+        }
     }
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProfileCard() {
+fun AppBar() {
+    TopAppBar(
+        navigationIcon = {
+            Icon(
+                Icons.Default.Home,
+                "content description",
+                Modifier.padding(horizontal = 12.dp)
+            )
+        },
+        title = { Text("Messaging Application users") },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.skyBlue,
+            titleContentColor = MaterialTheme.colorScheme.surface,
+            navigationIconContentColor = MaterialTheme.colorScheme.surface
+        )
+    )
+}
+
+
+@Composable
+fun ProfileCard(user: String, status: Boolean) {
     Card(
         modifier = Modifier
-            .padding(16.dp)
+            .padding(top = 12.dp, bottom = 4.dp, start = 16.dp, end = 16.dp)
             .fillMaxWidth()
             .wrapContentHeight(align = Alignment.Top),
-        elevation = CardDefaults.cardElevation(8.dp)
+        elevation = CardDefaults.cardElevation(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        ),
+        shape = CutCornerShape(topEnd = 24.dp)
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -66,7 +113,7 @@ fun ProfileCard() {
             horizontalArrangement = Arrangement.Start
         ) {
             ProfilePicture()
-            ProfileContent()
+            ProfileContent(user, status)
         }
     }
 }
@@ -75,7 +122,7 @@ fun ProfileCard() {
 fun ProfilePicture() {
     Card(
         shape = CircleShape,
-        border = BorderStroke(width = 2.dp, color = Green),
+        border = BorderStroke(width = 2.dp, color = MaterialTheme.colorScheme.lightGreen),
         modifier = Modifier.padding(16.dp),
         elevation = CardDefaults.cardElevation(4.dp)
 
@@ -92,17 +139,23 @@ fun ProfilePicture() {
 
 
 @Composable
-fun ProfileContent() {
+fun ProfileContent(userName: String, isActive: Boolean) {
+
+    val statusText = if (isActive) {
+        "Active Now"
+    } else {
+        "Offline Now"
+    }
     Column(
         modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth()
     ) {
         Text(
-            text = "John Doe", style = MaterialTheme.typography.headlineMedium,
+            text = userName, style = MaterialTheme.typography.headlineMedium,
         )
         Text(
-            text = "Active Now", style = MaterialTheme.typography.bodyLarge,
+            text = statusText, style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier.alpha(0.8f)
         )
     }
@@ -111,5 +164,7 @@ fun ProfileContent() {
 @Preview(showBackground = true)
 @Composable
 fun MainScreenPreview() {
-    MainScreen()
+    MyTheme {
+        MainScreen()
+    }
 }
--- ComposeApp/app/src/main/java/com/devdeveloper/composeapp/ui/theme/Theme.kt
+++ ComposeApp/app/src/main/java/com/devdeveloper/composeapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.devdeveloper.composeapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,22 +8,25 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    surface = veryLightGrey
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    surface = veryLightGrey
+
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
@@ -34,7 +36,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun ComposeAppTheme(
+fun MyTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
